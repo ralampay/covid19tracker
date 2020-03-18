88a5ee756d85d74ee5f7f6981faced10f059b4a6
Raphael Alampay <raphael.alampay@gmail.com>
2020-03-18 15:24:29 +0800

    updated root page table

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,9 @@
 class PagesController < ApplicationController
   def index
+    @patients = Patient.all
+
+    @patients = @patients.order("updated_at DESC")
+
+    @patients = @patients.page(params[:page]).per(20)
   end
 end
