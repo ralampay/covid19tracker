54ef5cd093cac5862d2c5c2831cc561df6eeccb6
Kathleen Zita Gayle Logmao <kathleenlogmao@gmail.com>
2020-03-19 22:35:07 +0800

    survey

--- app/controllers/administration/surveys_controller.rb
+++ app/controllers/administration/surveys_controller.rb
@@ -1,6 +1,58 @@
 module Administration
   class SurveysController < ApplicationController
     before_action :authenticate_user!
-    before_action :authenticate_admin!
+    # before_action :authenticate_admin!
+
+    def index
+      @surveys = Survey.all
+
+      @surveys = @surveys.order(
+                    "updated_at DESC"
+                  )
+
+      @surveys = @surveys.page(params[:page]).per(20)
+    end
+
+    def show
+      @survey = Survey.find(params[:id])
+    end
+
+
+    def new
+      @survey  = Survey.new
+    end
+
+    def create
+      @survey  = Survey.new(survey_params)
+
+      if @survey.save
+        redirect_to administration_survey_path(@survey)
+      else
+        render :new
+      end
+    end
+
+    def edit
+      @survey = Survey.find(params[:id])
+    end
+
+    def update
+      @survey = Survey.find(params[:id])
+      
+      if @survey.update(survey_params)
+        redirect_to administration_survey_path(@survey)
+      else
+        render :edit
+      end
+    end
+
+    def destroy
+      @survey.destroy!
+      redirect_to surveys_path
+    end
+
+    def survey_params
+      params.require(:survey).permit!
+    end
   end
 end
